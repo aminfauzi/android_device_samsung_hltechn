ebeb4121887ccf8f969c1e1d82db0ac289082d55
Joshua Primero <jprimero155@gmail.com>
2019-09-18 03:25:06 +0000

    [DNM]hlte: fix build
    Signed-off-by: Joshua Primero <jprimero155@gmail.com>

--- device.mk
+++ device.mk
@@ -23,8 +23,7 @@ $(call inherit-product-if-exists, vendor/samsung/hltechn/hltechn-vendor.mk)
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
